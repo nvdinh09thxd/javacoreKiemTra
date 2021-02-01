2dc3b9427ffe57b2ab014535591a629f4dff45ca
nvdinh185 <nvdinh0766777123@gmail.com>
2021-02-01 14:20:52 +0700

    delete son_han

--- src/son_han/BeansTamGiac/TamGiacCanException.java
+++
@@ -1,7 +0,0 @@
-package son_han.BeansTamGiac;
-
-public class TamGiacCanException extends Exception {
-	public TamGiacCanException(String msg) {
-		super(msg);
-	}
-}
--- src/son_han/beansthesong/TheSongException.java
+++
@@ -1,7 +0,0 @@
-package son_han.beansthesong;
-
-public class TheSongException extends Exception {
-	public TheSongException(String msg) {
-		super(msg);
-	}
-}
